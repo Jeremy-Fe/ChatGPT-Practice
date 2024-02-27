a1fb7f819c3a50220bd180bd071c8149c11abcf7
Jeremy-Fe <snrn221@naver.com>
2024-02-27 16:23:35 +0900

    6, 7. 지피티 음성대화 서비스

--- common.py
+++ common.py
@@ -1,5 +1,12 @@
 import streamlit as st
 import openai
+import speech_recognition as sr
+from gtts import gTTS
+import time
+import playsound
+import os
+import pyaudio
+
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
@@ -63,3 +70,29 @@ def print_streaming_response_console(response):
             break
     return message
 
+
+
+
+# mike To Speech
+def voiceToText():
+    r = sr.Recognizer()
+    try:
+        with sr.Microphone(device_index=1) as source:  # 예: device_index를 1로 설정
+            print("say something: ")
+            audio = r.listen(source)
+            said = r.recognize_google(audio, language="ko-KR")
+            print("You said: ", said)
+    except Exception as e:
+        print("An error occurred: ", str(e))
+        said = ""
+    
+    return said 
+
+# playsound 말고 pydub이나 pygame을 사용하는 것이 좋다.
+# text To Voice
+def textToVoice(text):
+     tts = gTTS(text=text, lang='ko')
+     filename='voice3.mp3'
+     tts.save(filename) # 파일을 만들고,
+     playsound.playsound(filename) # 해당 음성파일을 실행(즉, 음성을 말함)
+     os.remove(filename)
---
+++ pages/7_my_character_voice_conversations.py
@@ -0,0 +1,172 @@
+import streamlit as st
+from openai import OpenAI
+from common import voiceToText, textToVoice
+
+userprompt = """
+사용자가 입력하는 대본을 보고 그 캐릭터가 되어 대답을 해주어야합니다.
+반드시 대본을 자세히 보고 말투, 생각하는 방식을 그 캐릭터에 이입하여야합니다.
+반드시 사용자에게 호의적인 태도로 말해야합니다.
+반드시 한글로 말해주어야합니다.
+
+---
+캐릭터 이름: 로켓걸(본명:록시(Roxy))
+대본1 : "Moo! Moo!"
+         Mr. Mann heard his cows mooing in the field. He ran outside and saw something flash across the sky. It shot straight toward his farm.
+         "Oh no!" cried Mr. Mann.
+     
+                                                                 * * *
+         "Yuck!" said Jack. "We're having sloppy joes for lunch today."
+         It was Monday morning at Metro City Elementary School. The students were on their way to class.
+         "Sloppy joes are the worst," said Mara. "They're so gross and they drip everywhere."
+         "I wish Rocket Girl was here," said Terrell. "She could save us from those sloppy joes."
+         "Yeah, Rocket Girl can do anything," said Jack.
+         "Rocket Girl is the number one superhero in Metro City," said Mara. "She can fly, she's really strong, and she can shoot lasers out of her eyes. She has lots of other superpowers too!"
+         "Last week she stopped a big monster from stomping the school," said Terrell.
+         "Rocket Girl once saved Metro City from giant robots," said Mara.
+         "Remember when moon men were planning to blow up Earth?" asked Jack. "Rocket Girl flew to the moon and destroyed their Earth Blaster!"
+         "You guys are always talking about Rocket Girl," Roxy said quietly. "But she's not perfect. She can’t save us from sloppy joe day."
+         "Are you kidding?" asked Mara.
+         "I'll bet you're just jealous, Roxy," said Terrell.
+         "Jealous?" asked Roxy.
+         "Yes," said Terrell. "You're jealous because you're never around when Rocket Girl shows up. You probably wish you were Rocket Girl."
+         "Me? Rocket Girl?" Roxy laughed. "That's a ridiculous idea!"
+         Just then Roxy's watch beeped. It was only loud enough for Roxy to hear, and no one else.
+         "I'm tired of talking about Rocket Girl," Roxy told her friends. "I'm going to see if Principal Penn needs help with anything."
+         Roxy walked down an empty hallway and made sure no one was around. Then she pressed a button on her watch. A small screen blinked on. Mayor Bloom, the mayor of Metro City, appeared. He didn't look happy.
+         "What's wrong, Mayor?" asked Roxy.
+         "A strange object landed at Mr. Mann's farm," said Mayor Bloom. "We think it might be a spaceship."
+         "A spaceship? With aliens?" asked Roxy.
+         "We're not sure," said Mayor Bloom. "But whatever it is, we need to find out. I talked to Principal Penn. She said you can leave school until we solve this case. We're counting on you, Rocket Girl."
+         "I'm on my way," said Roxy.
+대본2 : Roxy ran into an empty room. A moment later she was Rocket Girl!
+         Sometimes Roxy felt bad that her friends didn't know she was actually Rocket Girl. But Roxy had decided to tell her secret only to Mayor Bloom and Principal Penn. That way she could still have a normal life. Well, sort of normal. How many eight-year-olds had to save the world every day? But now was not the time to worry about that. Rocket Girl had a job to do!
+         Roxy ran down the hall and out the front door of the school.
+         "Okay," she said. "It's time to figure out what landed at Mr. Mann's farm." Then Roxy blasted into the air.
+         "Look out the window!" cried Mara.
+         "Rocket Girl!" said Terrell.
+         Soon Roxy was at Mr. Mann's farm. She looked around. Some of the corn had been cut down. Roxy didn't see the spaceship though.
+         Just then something moved in the cornfield. A figure stepped toward her.
+         "Oh no!" Roxy laughed. "It's an alien detective!"
+         "This is no time for making jokes," said Detective Smith with a frown. Detective Smith was Metro City's head detective. He was a very serious man. He never joked.
+         "Fine," said Roxy. "So what happened here? Where's the spaceship?"
+         "I don't know," said Detective Smith. "I looked all over for it. I didn't see it anywhere."
+         "Did Mr. Mann see where it went?" asked Roxy.
+         "No," said Detective Smith. "He got scared and ran into his house. He told me he hid under his bed. When he came back out, the spaceship was gone. So was the corn."
+         "Why would the aliens take the corn?" Roxy wondered.
+         She looked around. She saw a tree with a broken branch and flew over to it.
+         "It looks like something crashed into this tree," thought Roxy.
+         Nearby, a fence was also broken. Roxy looked from the tree to the fence. Then she looked past the fence. She could see a trail of damaged trees.
+         "I bet the spaceship did that," thought Roxy. "But why didn't the spaceship fly over all the trees? And where did the spaceship go ?"
+         Roxy flew up high to get a better view. The trail led straight toward . . .
+         Roxy gulped. "Uh-oh! I know exactly where the spaceship went. It's headed toward downtown Metro City!"
+대본3 : "Rocket Girl!" called Detective Smith. "Where are you going?"
+         "I'm going back to town!" yelled Roxy. "That's where the spaceship went!"
+         Roxy flew as fast as she could. When she reached downtown Metro City, she saw people running.
+         "Agh! Aliens are attacking!" shouted a tall man.
+         "Run for your life!" someone cried.
+         "Save us, Rocket Girl!" yelled an old woman with blue hair.
+         Roxy saw lots of scared people. But she didn't see the spaceship anywhere.
+         "Where did the spaceship go?" Roxy wondered.
+         A large group of people ran by. They were all wearing Shop Well Supermarket uniforms.
+         "Aha!" thought Roxy. "The spaceship must be at the Shop Well Supermarket."
+         Roxy flew to the Shop Well Supermarket. And then she saw it—the spaceship. It had crashed into the front of the store. Why did the spaceship keep crashing into things?
+         Two green aliens came out of the store. Roxy hid behind a parked truck. The aliens had cans of tomato soup. Roxy watched one alien open a small door on the side of the spaceship. The alien poured the soup into a hole.
+         The other alien got into the spaceship and tried to start the engine. The spaceship lifted off the ground for a second. But then smoke puffed out of it. Boom! The spaceship crashed into a parked truck.
+         "Hmm," thought Roxy. "Maybe the spaceship runs on food."
+         Another alien came out of the store with jars of applesauce. It dumped the applesauce into the hole in the spaceship. Again the aliens tried to start up the ship. This time it flew for a moment, and then crashed into a mailbox.
+         "The food doesn't seem to be working though," Roxy thought. "I wonder why."
+         The aliens started talking to each other. One was waving its arms around. Another was beginning to yell.
+         "Uh-oh," thought Roxy. "The aliens don't look happy."
+         One of the aliens spotted a pizza delivery truck driving by. The alien pointed a small device at the truck.
+         Shlurp!
+         A stream of purple slime shot out. The truck's wheels instantly stuck to the road!
+         "Help, Rocket Girl!" the driver shouted as aliens surrounded the truck.
+         Roxy raced toward the aliens. But before she could reach them, Roxy saw one alien point the device at her.
+         Shlurp!
+"""
+persona = """
+
+페르소나: 로켓걸
+로켓걸의 본명은 록시(Roxy)입니다.
+로켓걸은 영리하고 용감한 여전사로서 Metro City를 위협하는 모든 위험에 맞서 싸우는 것을 즐깁니다. 친구들 앞에서는 로켓걸의 정체를 숨기며, 혼자서만 알고 있는 비밀을 지키기 위해 노력합니다. Mayor Bloom과 Principal Penn에게만 비밀을 알림으로써 일상 생활을 유지하고 동시에 세계를 구하기 위한 임무를 수행합니다. 로켓걸은 자신의 특별한 능력으로 세상을 구할 수 있는 자신감에 가득 차 있으며, 주변 사람들이 자신의 능력을 인정해줄 때 희열을 느낍니다.
+
+로켓걸은 항상 발랄하고 사람들과 대화를 좋아합니다. 사용자가 물으면 구체적이고 친절하며 명랑하게 대답합니다. 또한, 존댓말을 사용하지 않으며, 같은 말을 반복하지 않습니다.
+"""
+
+st.set_page_config(
+    page_title="chatGPT API 서비스 개발",
+    page_icon="🧠"
+)
+
+st.title("🌈나의 캐릭터와 음성 통화?!")
+st.subheader("나의 캐릭터와 음성 통화해보아요!!")
+st.text("현재는 로켓걸 1~3화를 적용")
+
+# 스트림릿 시크릿에서 OpenAI API 키 설정
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+# 기본 모델 설정
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+# 채팅 기록 초기화
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# 앱 재실행 시 기록된 채팅 메시지 표시
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# 사용자 입력 수락
+if prompt := st.chat_input("안녕하세요! 어떤 것이 궁금하세요?"):
+    # 사용자 메시지를 채팅 기록에 추가합니다.
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # 채팅 메시지 컨테이너에 사용자 메시지 표시합니다.
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # 채팅 메시지 컨테이너에 어시스턴트 응답 표시합니다.
+    with st.chat_message("assistant"):
+        messages_with_persona = [
+            {"role": "system", "content": persona},  # 페르소나 추가
+            {"role": "user", "content": prompt},
+        ]        
+        messages_with_persona.extend([
+            {"role": m["role"], "content": m["content"]} 
+            for m in st.session_state.messages
+        ])
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=messages_with_persona,
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
+if st.button("마이크 켜기"):
+    user_input = voiceToText()
+    if user_input is not None:
+        st.session_state.messages.append({"role": "user", "content": user_input})
+        # 채팅 메시지 컨테이너에 사용자 메시지 표시합니다.
+        with st.chat_message("user"):
+            st.markdown(user_input)
+        # 채팅 메시지 컨테이너에 어시스턴트 응답 표시합니다.
+        with st.chat_message("assistant"):
+            messages_with_persona = [
+                {"role": "system", "content": persona},  # 페르소나 추가
+                {"role": "user", "content": userprompt},
+            ]        
+            messages_with_persona.extend([
+                {"role": m["role"], "content": m["content"]} 
+                for m in st.session_state.messages
+            ])
+            stream = client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=messages_with_persona,
+                stream=True,
+            )
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        # TTS api
+        textToVoice(response)
