2a6c827246878de784148a530a76b81896ff79dd
Jeremy-Fe <snrn221@naver.com>
2024-02-29 10:56:43 +0900

    test

--- common.py
+++ common.py
@@ -89,7 +89,7 @@ def voiceToText():
 # text To Voice
 # 한국어 id : HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_KO-KR_HEAMI_11.0
 # 영어 ID : HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0
-def textToVoice(text='hi'):
+def textToVoice(text):
     voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
     engine = pyttsx3.init()
     engine.setProperty('voice', voice_id)
